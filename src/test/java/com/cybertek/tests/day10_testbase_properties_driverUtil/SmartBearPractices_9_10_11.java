db4b4195f31552261960bbf676e4e0c0f8ff06ad
Liliya Kaplan <gafifettey@gmail.com>
2020-11-15 10:28:38 -0500

    day 10 edited, testBase added

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices_9_10_11.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices_9_10_11.java
@@ -1,39 +1,32 @@
 package com.cybertek.tests.day10_testbase_properties_driverUtil;
 
+import com.cybertek.tests.base.TestBase;
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.SmartBearUtilities;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
-public class SmartBearPractices_9_10_11 {
-
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setupMethod(){
-        driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        SmartBearUtilities.loginToSmartBear(driver);
-    }
+public class SmartBearPractices_9_10_11 extends TestBase {
 
     @Test
     public void p9_delete_order_task(){
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        SmartBearUtilities.loginToSmartBear(driver);
         WebElement markSmithCheckBox = driver.findElement(By.xpath("//td[.='Mark Smith']/preceding-sibling::td[1]"));
         markSmithCheckBox.click();
+        BrowserUtils.wait(1);
         WebElement deleteButton = driver.findElement(By.id("ctl00_MainContent_btnDelete"));
         deleteButton.click();
 
-        WebElement names = driver.findElement(By.xpath("//table[@id='ctl00_MainContent_orderGrid']"));
-        Assert.assertFalse(names.getText().equals("Mark Smith"), "MarkSmith is still in the list. Verification failed");
-
+        //SmartBearUtilities.verifyOrder(driver, "Mark Smith");
+        List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+        for (WebElement each : namesList) {
+            Assert.assertFalse(each.getText().equals("Mark Smith"));
+        }
 
     }
 
